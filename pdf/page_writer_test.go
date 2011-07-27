d1f5f6c2c048a7ca9c12030dce29f048ca335693
Brent Rowland <brent.rowland@ttiltd.com>
2011-07-27 14:34:14 -0700

    Alphabetize page writer tests

--- pdf/page_writer_test.go
+++ pdf/page_writer_test.go
@@ -5,26 +5,15 @@ import (
 	"bytes"
 )
 
-func TestPageWriter_units(t *testing.T) {
+func TestPageWriter_MoveTo(t *testing.T) {
 	var buf bytes.Buffer
 	dw := NewDocWriter(&buf)
-	// defaults to points
-	pw1 := newPageWriter(dw, Options{})
-	expectS(t, "pt", pw1.units.name)
-	expectF(t, 1, pw1.units.ratio)
-	// inches
-	pw2 := newPageWriter(dw, Options{"units": "in"})
-	expectS(t, "in", pw2.units.name)
-	expectF(t, 72, pw2.units.ratio)
-	// centimeters
-	pw3 := newPageWriter(dw, Options{"units": "cm"})
-	expectS(t, "cm", pw3.units.name)
-	expectF(t, 28.35, pw3.units.ratio)
-	// custom: "Dave points"
-	UnitConversions.Add("dp", 0.072)
-	pw4 := newPageWriter(dw, Options{"units": "dp"})
-	expectS(t, "dp", pw4.units.name)
-	expectF(t, 0.072, pw4.units.ratio)
+	pw := newPageWriter(dw, Options{})
+	expectF(t, 0, pw.loc.x)
+	expectF(t, 0, pw.loc.y)
+	pw.MoveTo(222, 333)
+	expectF(t, 222, pw.loc.x)
+	expectF(t, 792 - 333, pw.loc.y)
 }
 
 func TestPageWriter_PageHeight(t *testing.T) {
@@ -70,13 +59,24 @@ func TestPageWriter_translate(t *testing.T) {
 	expectF(t, 792 - 200, loc.y)
 }
 
-func TestPageWriter_MoveTo(t *testing.T) {
+func TestPageWriter_units(t *testing.T) {
 	var buf bytes.Buffer
 	dw := NewDocWriter(&buf)
-	pw := newPageWriter(dw, Options{})
-	expectF(t, 0, pw.loc.x)
-	expectF(t, 0, pw.loc.y)
-	pw.MoveTo(222, 333)
-	expectF(t, 222, pw.loc.x)
-	expectF(t, 792 - 333, pw.loc.y)
+	// defaults to points
+	pw1 := newPageWriter(dw, Options{})
+	expectS(t, "pt", pw1.units.name)
+	expectF(t, 1, pw1.units.ratio)
+	// inches
+	pw2 := newPageWriter(dw, Options{"units": "in"})
+	expectS(t, "in", pw2.units.name)
+	expectF(t, 72, pw2.units.ratio)
+	// centimeters
+	pw3 := newPageWriter(dw, Options{"units": "cm"})
+	expectS(t, "cm", pw3.units.name)
+	expectF(t, 28.35, pw3.units.ratio)
+	// custom: "Dave points"
+	UnitConversions.Add("dp", 0.072)
+	pw4 := newPageWriter(dw, Options{"units": "dp"})
+	expectS(t, "dp", pw4.units.name)
+	expectF(t, 0.072, pw4.units.ratio)
 }
