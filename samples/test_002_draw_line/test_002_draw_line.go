b95f939b55171d0eca564c259899607a15e4c499
Brent Rowland <brent.rowland@ttiltd.com>
2011-08-08 18:05:00 -0700

    Sample demonstrating drawing a line.

---
+++ samples/test_002_draw_line/test_002_draw_line.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"../../pdf/_obj/go-pdf/pdf"
+	"os"
+	"exec"
+)
+
+const name = "test_002_draw_line.pdf"
+
+func main() {
+	f, err := os.Create(name)
+	if err != nil {
+		panic(err)
+	}
+	doc := pdf.NewDocWriter(f)
+	doc.Open(pdf.Options{})
+	doc.OpenPage(pdf.Options{})
+
+	doc.SetLineColor(pdf.Blue)
+	doc.SetLineWidth(3, "pt")
+	doc.SetLineDashPattern("dashed")
+
+	doc.SetUnits("in")
+	doc.MoveTo(1, 1)
+	doc.LineTo(2, 2)
+
+	doc.ClosePage()
+	doc.Close()
+	f.Close()
+	exec.Command("open", name).Start()
+}
