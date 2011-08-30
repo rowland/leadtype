8ad4f76ef5710ba76131548b4df8af43cebd2fd3
Brent Rowland <brent.rowland@ttiltd.com>
2011-08-29 18:38:57 -0700

    Dump individual features of fonts.

--- ttdump/ttdump.go
+++ ttdump/ttdump.go
@@ -10,9 +10,15 @@ func main() {
 	fontname := os.Args[1]
 	fmt.Println("ttdump", fontname)
 	font, err := ttf.LoadFont(fontname)
-	if err == nil {
-		fmt.Print(font)
-	} else {
+	if err != nil {
 		fmt.Println(err)
+		os.Exit(-1)
+	}
+	if len(os.Args) < 3 {
+		font.Dump(os.Stdout, "all")
+	} else {
+		for i := 2; i < len(os.Args); i++ {
+			font.Dump(os.Stdout, os.Args[i])
+		}
 	}
 }
