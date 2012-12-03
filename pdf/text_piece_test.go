316ff5a864a2c58f5251c2d1cbd7edf382885fda
Brent Rowland <brent.rowland@ttiltd.com>
2012-12-02 16:00:51 -0800

    Missing tests for TextPiece.

--- pdf/text_piece_test.go
+++ pdf/text_piece_test.go
@@ -5,6 +5,15 @@ package pdf
 
 import "testing"
 
+func textPieceTestText() *TextPiece {
+	font := testTtfFonts("Arial")[0]
+	return &TextPiece{
+		Text:     "Lorem",
+		Font:     font,
+		FontSize: 10,
+	}
+}
+
 func TestTextPiece_measure(t *testing.T) {
 	font := testTtfFonts("Arial")[0]
 	piece := &TextPiece{
@@ -16,6 +25,21 @@ func TestTextPiece_measure(t *testing.T) {
 	expectNF(t, "Width", 58.05, piece.Width)
 	expectNI(t, "Chars", 5, piece.Chars)
 	expectNI(t, "Tokens", 1, piece.Tokens)
+}
+
+func TestTextPiece_Ascent(t *testing.T) {
+	piece := textPieceTestText()
+	expectF(t, 18.54, piece.Ascent())
+}
+
+func TestTextPieceDescent(t *testing.T) {
+	piece := textPieceTestText()
+	expectF(t, -4.34, piece.Descent())
+}
+
+func TestTextPiece_Height(t *testing.T) {
+	piece := textPieceTestText()
+	expectFdelta(t, 22.88, piece.Height(), 0.001)
 }
 
 // 313 ns
