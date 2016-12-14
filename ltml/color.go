f483c8cd839666884f8773d98e7089b0ec8b9202
Brent Rowland <brent.rowland@ttiltd.com>
2016-12-13 23:27:58 -0800

    Remove 3-digit hex color option. Strip # from hex colors. Punt color validation to PDF library.

--- ltml/color.go
+++ ltml/color.go
@@ -8,20 +8,16 @@ import (
 )
 
 var (
-	re6DigitHexColor = regexp.MustCompile(`^#([0-9A-Fa-f]{6})$`)
-	re3DigitHexColor = regexp.MustCompile(`^#([0-9A-Fa-f]{3})$`)
+	re6DigitHexColor = regexp.MustCompile(`#([0-9A-Fa-f]{6})`)
 )
-
-func validColor(color string) bool {
-	return re6DigitHexColor.MatchString(color) || re3DigitHexColor.MatchString(color)
-}
 
 type Color string
 
 func (c *Color) SetAttrs(attrs map[string]string) {
 	if color, ok := attrs["color"]; ok {
-		if validColor(color) {
-			*c = Color(color)
+		if matches := re6DigitHexColor.FindStringSubmatch(color); len(matches) >= 2 {
+			color = matches[1]
 		}
+		*c = Color(color)
 	}
 }
